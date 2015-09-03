14c8b5cb4bdf218223fba7371bfcc0dcbcfbf1ab
Bharat Arimilli <bbarimilli@outlook.com>
2015-09-02 23:41:07 -0500

    Initial Commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,16 @@
+/**
+ * Main JS file for Spark behaviours
+ */
+
+/* globals jQuery, document */
+(function ($, undefined) {
+    "use strict";
+
+    var $document = $(document);
+
+    $document.ready(function () {
+
+    
+
+    });
+})(jQuery);
